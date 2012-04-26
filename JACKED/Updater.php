55b4275eb373b29cd5b056ea5eff1ac6f4d61dac
Nick Pettazzoni <pettazz@gmail.com>
2012-04-26 14:30:45 -0400

    added some backup methods to updater

--- JACKED/Updater.php
+++ JACKED/Updater.php
@@ -1,4 +1,5 @@
 <?php
+
     /**
     * Your one-stop shop for updating JACKED modules and core
     */
@@ -9,12 +10,56 @@
 
 
         /**
-        * Get 
+        * Create a Backup of the entire JACKED folder as it exists right now in the backups folder.
         * 
-        * @param int $user User ID to check friendship with
-        * @throws NotLoggedInException if the user is not logged in
-        * @return Boolean Whether the user is friends with the given user 
+        * @param $appendName String [optional] String to append to the tarball name. Defaults to nothing.
+        * @return String The filename of the newly created backup tarball
         */
-        
+        private function createBackup($appendName = false){
+            if(!is_dir(JACKED_MODULES_ROOT . 'backups')){
+                mkdir(JACKED_MODULES_ROOT . 'backups');
+            }
 
+            $name = time() . ($appendName? '.' . $appendName : '') . '.tar';
+            $phar = new PharData($name); 
+            $phar->buildFromDirectory(JACKED_MODULES_ROOT, '/^((?!backups).)*$/');
+            $this->JACKED->Logr->write('Created a backup called ' . $name);
+
+            return $name;
+        }
+        
+        /**
+        * Restores a backup of the JACKED folder. Makes a new backup of the current folder 
+        * marked with 'current_from_restore' first. 
+        * 
+        * @param $name String [optional] The name of the backup tarball to restore. Defaults to the most recent.
+        * @return Boolean Whether the restore was completed successfully.
+        */
+        private function createBackup($name = false){
+            if(!is_dir(JACKED_MODULES_ROOT . 'backups')){
+                return false;
+            }
+
+            $current = $this->createBackup('current_from_restore');
+            try{
+                $backups = scandir(JACKED_MODULES_ROOT . 'backups', 1);
+                if($backups[0] == $current){
+                    if($backups[1] == '..'){
+                        return false;
+                    }else{
+                        $name = $backups[1];
+                    }
+                }else{
+                    $name = $backups[0];
+                }
+                $phar = new PharData(JACKED_MODULES_ROOT . 'backups' . $name);
+                $done = $phar->extractTo(JACKED_MODULES_ROOT, null, true);
+                $this->JACKED->Logr->write('Restored from a backup called ' . $name);
+                return $done;
+
+            }catch(Exception $e){
+                $this->JACKED->Logr->write('Failed to restore backup (' . $name . '): ' . $e->getMessage() , 2);
+                return false;
+            }
+        }
 ?>
