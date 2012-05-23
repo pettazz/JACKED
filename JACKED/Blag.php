538c4c19a732eea0b8c42b0227a73d6c9b467d6b
Nick Pettazzoni <pettazz@gmail.com>
2012-05-23 16:58:28 -0400

    blag module stubs

---
+++ JACKED/Blag.php
@@ -0,0 +1,15 @@
+<?php
+
+	class Blag extends JACKEDModule{
+		/*
+			
+		*/
+        
+        const moduleName = 'Blag';
+        const moduleVersion = 1.0;
+        public static $dependencies = array('MySQL', 'Flock' => array('required' => false));
+        
+        
+	}
+
+?>
---
+++ JACKED/conf/Blag.php
@@ -0,0 +1,8 @@
+<?php
+
+    $settings = array(
+    	
+    
+    );
+
+?>
