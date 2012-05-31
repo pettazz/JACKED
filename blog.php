fcfae2ee783c7d1707b29b49f2c9565411b7cb18
Nick Pettazzoni <pettazz@gmail.com>
2012-05-31 11:54:51 -0400

    added some EYS measuring to the blag test

--- blog.php
+++ blog.php
@@ -1,8 +1,9 @@
 <?php
 
     require('jacked_conf.php');
-    $JACKED = new JACKED("Blag");
+    $JACKED = new JACKED(array("Blag", "EYS"));
     $blog = $JACKED->Blag;
+    $eys = $JACKED->EYS;
 
 ?>
 <!DOCTYPE html>
@@ -12,11 +13,16 @@
 </head>
 
 <body>
-
-<?php
-	print_r($blog->getPost('123-test'));	
-?>
-
+	<pre><code>
+	<?php
+		$eys->setMark('getposts');
+		$posts = $blog->getPost('123-test')
+		$eys->setMark('getposts_end');
+		print_r($posts);	
+		$timer = $eys->getDelta('getposts', 'getposts_end');
+	?>
+	</code></pre>
+	<h4><?php echo 'took: ' . round(($timer['time'] * 1000), 4) . 'ms and increased allocated memory by ' . round($timer['memory'] / 2048, 4) . ' MB'; ?></h4>
 </body>
 
 </html>
