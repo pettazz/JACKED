8637713bec992a9c7ed08b09b7665ba548c3cfbf
Nick Pettazzoni <pettazz@gmail.com>
2012-05-31 09:24:44 -0400

    Added posts table and author name type options

--- JACKED/conf/Blag.php
+++ JACKED/conf/Blag.php
@@ -1,7 +1,15 @@
 <?php
 
     $settings = array(
-    	
+        
+        'dbt_posts' => 'BlagPosts', 
+
+        //what to get for author names. one of:
+        // full - "Steve Duderson"
+        // first - "Steve"
+        // user - "supersteve2210"
+        //defaults to 'first' if not set or invalid
+        'author_name_type' => 'full'
     
     );
 
