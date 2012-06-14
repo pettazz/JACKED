0d04dce121bbc71c30cf9f367c325078ab97b2e3
Nick Pettazzoni <pettazz@gmail.com>
2012-06-14 14:23:59 -0400

    this is trouble

---
+++ JACKED/JACKEDContentModule.php
@@ -0,0 +1,14 @@
+<?php
+
+    abstract class JACKEDContentModule extends JACKEDModule{
+        /*
+            HA HAA
+
+            WHERE ARE WE?
+        */
+            
+
+        
+    }
+?>
+        
