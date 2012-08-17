d40f096e157eb5a9a27fc66a6249b7b35e1359c5
Nick Pettazzoni <pettazz@gmail.com>
2012-08-17 15:36:44 -0400

    cleanup, enable all getters/setters for config objects

--- JACKED/Configur.php
+++ JACKED/Configur.php
@@ -5,36 +5,6 @@
         const moduleVersion = 1.5;
     
         protected $_values = array();
-    
-        //I TOOK YOUR SINGLETON PATTERN/DEPENDENCY INJECTION AND ノ(°□°ノ）
-        
-        //protected static $_instance = null;
-        //protected static $_modules = array();
-        /*public static function getInstance(){
-            if (self::$_instance === null) {
-                self::$_instance = new Configur;
-            }
-
-            return self::$_instance;
-        }*/
-        /*public function loadConf($name){
-            //if we've already loaded this conf, dont do it again
-            if(!in_array($name, self::$_modules)){
-                $file = JACKED_CONFIG_ROOT . $name . '.php';
-                if (file_exists($file)){
-                    include($file);
-                    foreach($settings as $opt => $val){
-                        $this->_values[$opt] = $val;
-                    }
-                    array_push(self::$_modules, $name);
-                }else{
-                    throw new Exception("JACKED Configurator can't find a config file named " . $name . ".");
-                }
-            }
-        }*/
-        
-        //ABRA KADABRA! ヽ( ﾟヮ・)ノ.･ﾟ*｡･+☆ NOW IT MAKES FUCKING SENSE INSTEAD!
-        ////this crap will go in the trash someday but it's here for possible sanity later
         
         public function __construct($name){
             $file = JACKED_CONFIG_ROOT . $name . '.php';
@@ -46,10 +16,6 @@
             }else{
                 throw new Exception("JACKED Configurator can't find a config file named " . $name . ".");
             }
-        }
-        
-        final protected function __clone(){
-            //no thanks brah
         }
         
         public function count(){
@@ -65,13 +31,11 @@
         }
         
         public function offsetSet($offset, $value){
-            //these are "constants"
             $this->_values[$offset] = $value;
         }
         
         public function offsetUnset($offset){
-            //these are "constants"
-            //unset($this->_values[$offset]);
+            unset($this->_values[$offset]);
         }
         
         public function getIterator(){
@@ -79,8 +43,7 @@
         }
         
         public function __set($key, $value){
-            //these are "constants"
-            //$this->_values[$key] = $value;
+            $this->_values[$key] = $value;
         }
 
         public function __get($key){
