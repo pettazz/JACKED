131ddae18c482657dff4c0cfda67f46d265765a4
Nick Pettazzoni <pettazz@gmail.com>
2012-08-21 01:21:37 -0400

    ow my eyes

--- syruptest.php
+++ syruptest.php
@@ -1,9 +1,9 @@
 <?php
-    
+    <pre><code>
     require('jacked_conf.php');
     $JACKED = new JACKED("Syrup, Blag");
     //$JACKED->Syrup->registerModule('Blag');
 
     echo $JACKED->Syrup->Blag->count(array('alive' => '1')) . " blog posts exist.";
-
+    </pre></code>
 ?>
