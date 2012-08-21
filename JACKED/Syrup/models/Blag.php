17e833ec98025b78911eccbe36db801ae8082faf
Nick Pettazzoni <pettazz@gmail.com>
2012-08-21 01:19:10 -0400

    parent needs fields defined already

--- JACKED/Syrup/models/Blag.php
+++ JACKED/Syrup/models/Blag.php
@@ -13,8 +13,6 @@
         public $content;
 
         public function __construct($config, $logr){
-            parent::__construct($config, $logr);
-
             $this->$guid = new SyrupField(SyrupField::VARCHAR, 64, false, NULL, 'PRI');
             $this->$author = new SyrupField(SyrupField::VARCHAR, 64, false, NULL, 'FK');
             $this->$posted = new SyrupField(SyrupField::INT, 11, false);
@@ -22,6 +20,8 @@
             $this->$title = new SyrupField(SyrupField::VARCHAR, 255);
             $this->$headline = new SyrupField(SyrupField::TEXT, NULL, true, '');
             $this->$content = new SyrupField(SyrupField::TEXT);
+
+            parent::__construct($config, $logr);
         }
 
     }
