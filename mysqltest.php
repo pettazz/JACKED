d4557f3921a6f7c05782b9b4845d0e4b676bf292
Nick Pettazzoni <pettazz@gmail.com>
2012-08-21 15:14:41 -0400

    something to compare Syrup against

--- mysqltest.php
+++ mysqltest.php
@@ -4,12 +4,12 @@
     
     
     
-    //syruptest.php
+    //mysqltest.php
     
     require('jacked_conf.php');
-    $JACKED = new JACKED(array("Syrup", "EYS", "Blag"));
+    $JACKED = new JACKED(array("MySQL", "Blag", "EYS"));
     $JACKED->EYS->setMark('count_start');
-    echo $JACKED->Syrup->Blag->count(array('alive' => '1')) . " blog posts exist.";
+    echo $JACKED->MySQL->query('SELECT COUNT(*) FROM ' . $JACKED->Blag->config->dbt_posts . ' WHERE alive = 1') . " blog posts exist.";
     $JACKED->EYS->setMark('count_stop');
     $timer = $JACKED->EYS->getDelta('count_start', 'count_stop');
     echo '<br /><br />Count took: ' . round(($timer['time'] * 1000), 4) . 'ms and increased allocated memory by ' . round($timer['memory'] / 2048, 4) . ' MB.<br /><br />';
