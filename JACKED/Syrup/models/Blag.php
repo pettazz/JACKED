55b052bf702b3e8ad60afb061bcb1decd6d7c009
Nick Pettazzoni <pettazz@gmail.com>
2012-08-21 01:19:38 -0400

    /facepalm

--- JACKED/Syrup/models/Blag.php
+++ JACKED/Syrup/models/Blag.php
@@ -4,22 +4,14 @@
 
         const tableName = 'BlagPost';
 
-        public $guid;
-        public $author;
-        public $posted;
-        public $alive;
-        public $title;
-        public $headline;
-        public $content;
-
         public function __construct($config, $logr){
-            $this->$guid = new SyrupField(SyrupField::VARCHAR, 64, false, NULL, 'PRI');
-            $this->$author = new SyrupField(SyrupField::VARCHAR, 64, false, NULL, 'FK');
-            $this->$posted = new SyrupField(SyrupField::INT, 11, false);
-            $this->$alive = new SyrupField(SyrupField::TINYINT, 1, true, 1);
-            $this->$title = new SyrupField(SyrupField::VARCHAR, 255);
-            $this->$headline = new SyrupField(SyrupField::TEXT, NULL, true, '');
-            $this->$content = new SyrupField(SyrupField::TEXT);
+            $this->guid = new SyrupField(SyrupField::VARCHAR, 64, false, NULL, 'PRI');
+            $this->author = new SyrupField(SyrupField::VARCHAR, 64, false, NULL, 'FK');
+            $this->posted = new SyrupField(SyrupField::INT, 11, false);
+            $this->alive = new SyrupField(SyrupField::TINYINT, 1, true, 1);
+            $this->title = new SyrupField(SyrupField::VARCHAR, 255);
+            $this->headline = new SyrupField(SyrupField::TEXT, NULL, true, '');
+            $this->content = new SyrupField(SyrupField::TEXT);
 
             parent::__construct($config, $logr);
         }
