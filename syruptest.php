dcfba65fca2e528054769e8d3844a875d2186061
Nick Pettazzoni <pettazz@gmail.com>
2012-08-21 00:46:47 -0400

    testing lazy registration

--- syruptest.php
+++ syruptest.php
@@ -1,8 +1,8 @@
 <?php
     
     require('jacked_conf.php');
-    $JACKED = new JACKED("Syrup");
-    $JACKED->Syrup->registerModule('Blag');
+    $JACKED = new JACKED("Syrup, Blag");
+    //$JACKED->Syrup->registerModule('Blag');
 
     echo $JACKED->Syrup->Blag->count(array('alive' => '1')) . " blog posts exist.";
 
