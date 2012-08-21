89f5313cf97282dab2d1d73bb9cbacea746322ce
Nick Pettazzoni <pettazz@gmail.com>
2012-08-21 01:09:06 -0400

    order matters

--- JACKED/Syrup.php
+++ JACKED/Syrup.php
@@ -17,8 +17,8 @@
             $this->registeredModels = array();
 
             //import the base classes and correct SyrupDriver based on the driver name
-            include($this->config->model_root . 'SyrupModel.php');
             include($this->config->driver_root . 'SyrupDriverInterface.php');
+            include($this->config->model_root . 'SyrupModel.php');
             include($this->config->driver_root . $this->config->storage_driver_name . '.php');
 
             //turn on auto-registration if it's enabled
