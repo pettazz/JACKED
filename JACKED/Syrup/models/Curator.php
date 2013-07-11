8e9e5fd6e812f3817fb34b0e2cf5fe655dafe058
Nick Pettazzoni <pettazz@gmail.com>
2013-07-10 21:42:19 -0400

    I dunno why I even bothered

---
+++ JACKED/Syrup/models/Curator.php
@@ -0,0 +1,12 @@
+<?php
+
+    class CuratorModel extends SyrupModel{
+
+        const tableName = 'Curator';
+
+        protected $guid = array(SyrupField::VARCHAR, 64, false, NULL, 'PRI', NULL, array('UUID'));
+        protected $name = array(SyrupField::VARCHAR, 255, False);
+        protected $usage = array(SyrupField::INT, 11, True, 0);
+    }
+    
+?>
