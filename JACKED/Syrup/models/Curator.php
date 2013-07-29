e5ee52a16b8c261ff237a51f2bff479e3e83b62a
Nick Pettazzoni <pettazz@gmail.com>
2013-07-28 21:10:18 -0400

    This is really stupid and not going to stay this way
    I hope

--- JACKED/Syrup/models/Curator.php
+++ JACKED/Syrup/models/Curator.php
@@ -7,6 +7,10 @@
         protected $guid = array(SyrupField::VARCHAR, 64, false, NULL, 'PRI', NULL, array('UUID'));
         protected $name = array(SyrupField::VARCHAR, 255, False);
         protected $usage = array(SyrupField::INT, 11, True, 0);
+
+        protected function relationalGet($target){
+            return $this->query('SELECT * FROM Tag JOIN TagRel ON Tag.id = TagRel.Tag WHERE TagRel.target = ' $target);
+        }
     }
     
 ?>
