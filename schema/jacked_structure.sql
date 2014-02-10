e916b62ec9b63078f4f60dd2fc0a441f80efaa63
Nick Pettazzoni <pettazz@gmail.com>
2014-02-09 21:31:41 -0500

    clean up schema a bit

--- schema/jacked_structure.sql
+++ schema/jacked_structure.sql
@@ -4,20 +4,7 @@
 #
 # http://www.sequelpro.com/
 # http://code.google.com/p/sequel-pro/
-#
-# Host: nickpettazzoni.com (MySQL 5.5.22-0ubuntu1)
-# Database: jacked
-# Generation Time: 2012-09-05 20:16:55 +0000
 # ************************************************************
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
 # Dump of table admin_users
@@ -27,7 +14,7 @@ CREATE TABLE `admin_users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `User` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 
@@ -40,7 +27,7 @@ CREATE TABLE `Application` (
   `apiKey` varchar(64) NOT NULL DEFAULT '',
   `device` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`guid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 
@@ -59,7 +46,7 @@ CREATE TABLE `Blag` (
   PRIMARY KEY (`guid`),
   FULLTEXT KEY `title` (`title`),
   FULLTEXT KEY `content` (`content`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
 
@@ -70,7 +57,30 @@ CREATE TABLE `BlagCategory` (
   `guid` varchar(64) NOT NULL DEFAULT '',
   `name` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`guid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+# Dump of table Curator
+# ------------------------------------------------------------
+
+CREATE TABLE `Curator` (
+  `guid` varchar(64) NOT NULL DEFAULT '',
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `canonicalName` varchar(255) NOT NULL DEFAULT '',
+  `usage` int(11) DEFAULT 0,
+  PRIMARY KEY (`guid`),
+  FULLTEXT KEY `name` (`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table CuratorRelation
+# ------------------------------------------------------------
+
+CREATE TABLE `CuratorRelation` (
+  `Curator` varchar(64) NOT NULL DEFAULT '',
+  `target` varchar(64) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 
@@ -84,7 +94,7 @@ CREATE TABLE `Karma` (
   `weight` int(10) NOT NULL,
   `timestamp` int(11) NOT NULL,
   PRIMARY KEY (`guid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 
@@ -99,7 +109,7 @@ CREATE TABLE `Logr` (
   `line` int(10) DEFAULT NULL,
   `stack_hash` varchar(256) DEFAULT NULL,
   PRIMARY KEY (`guid`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
 
@@ -111,7 +121,7 @@ CREATE TABLE `sessions` (
   `access` int(10) NOT NULL DEFAULT '0',
   `data` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
 
@@ -126,7 +136,7 @@ CREATE TABLE `Source` (
   `bans` tinyint(14) DEFAULT '0',
   `data` text,
   PRIMARY KEY (`guid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 
@@ -141,35 +151,4 @@ CREATE TABLE `User` (
   `last_name` varchar(255) DEFAULT NULL,
   `first_name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`guid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-# Dump of table Curator
-# ------------------------------------------------------------
-
-CREATE TABLE `Curator` (
-  `guid` varchar(64) NOT NULL DEFAULT '',
-  `name` varchar(255) NOT NULL DEFAULT '',
-  `canonicalName` varchar(255) NOT NULL DEFAULT '',
-  `usage` int(11) DEFAULT 0,
-  PRIMARY KEY (`guid`),
-  FULLTEXT KEY `name` (`name`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
-
-
-# Dump of table CuratorRelation
-# ------------------------------------------------------------
-
-CREATE TABLE `CuratorRelation` (
-  `Curator` varchar(64) NOT NULL DEFAULT '',
-  `target` varchar(64) NOT NULL DEFAULT ''
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
