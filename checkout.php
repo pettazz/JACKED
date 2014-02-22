dc0fea22f1ac8a442e0443b37b40c64cb159693c
Nick Pettazzoni <pettazz@gmail.com>
2014-02-22 12:40:51 -0500

    someday ill rewrite Flock to not suck

--- checkout.php
+++ checkout.php
@@ -3,7 +3,7 @@
     header("Expires: Sat, 17 Jan 1998 05:00:00 GMT");
 
     require('jacked_conf.php');
-    $JACKED = new JACKED('Purveyor', 'Flock', 'Syrup');
+    $JACKED = new JACKED('Purveyor', 'Syrup');
 
     try{
         $product = $_POST['product'];
@@ -16,17 +16,14 @@
             $tickets = explode($tickets, ",");
         }
 
-        try{
-            $user = $JACKED->Flock->getUser($email);
-            $userid = $user['guid'];
-        }catch(UserNotFoundException $e){
-            $newUser = $JACKED->Syrup->User->create();
-            $newUser->email = $email;
-            $newUser->save();
-            $userid = $newUser->guid;
+        $user = $JACKED->Syrup->User->findOne(array('email' => $email));
+        if(!$user){
+            $user = $JACKED->Syrup->User->create();
+            $user->email = $email;
+            $user->save();    
         }
 
-        $result = $JACKED->Purveyor->createSale($userid, $product, $quantity, $method, $JACKED->config->base_url . 'JACKED/tomhanks.php', 'LOL, ETC.', $tickets);
+        $result = $JACKED->Purveyor->createSale($user->guid, $product, $quantity, $method, $JACKED->config->base_url . 'JACKED/tomhanks.php', 'LOL, ETC.', $tickets);
 
         header('Location: ' . $result['url']);
 
