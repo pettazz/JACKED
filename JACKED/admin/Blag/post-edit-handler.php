9ebc4ea60c7f3e3561f06ac4d8f4cef6f22d05fc
Nick Pettazzoni <pettazz@gmail.com>
2015-02-01 02:54:05 -0500

    fix blag edit handler dropping thumb changes

--- JACKED/admin/Blag/post-edit-handler.php
+++ JACKED/admin/Blag/post-edit-handler.php
@@ -51,6 +51,7 @@
                     $existingPost->alive = ($_POST['saveType'] == 'live'? 1 : 0);
                     $existingPost->title = $_POST['inputTitle'];
                     $existingPost->headline = $_POST['inputHeadline'];
+                    $existingPost->thumbnail = $_POST['inputThumbnail'];
                     $existingPost->content = $markdown->toHTML($_POST['inputContent']);
 
                     // three cases for tags
