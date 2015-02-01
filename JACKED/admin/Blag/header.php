705c0a80d4aef9b2730f0d3973daaa5fc11a33aa
Nick Pettazzoni <pettazz@gmail.com>
2015-02-01 15:24:12 -0500

    make 1.1 real, and ugh the admin webapp needs refactoring

--- JACKED/admin/Blag/header.php
+++ JACKED/admin/Blag/header.php
@@ -7,7 +7,7 @@
 <div class="hero-unit">
     <div class="row">
         <div class="span5 offset1">
-            <h2>Blag v1.0</h2>
+            <h2>Blag v1.1</h2>
         </div>
         <div class="span2 offset1">
             <p>Manage your Blag posts and junk.</p>
