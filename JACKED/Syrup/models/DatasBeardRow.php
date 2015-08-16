917792a6b0abf1f19453243ea420238291ffd8dc
Nick Pettazzoni <pettazz@gmail.com>
2015-08-16 01:58:19 -0400

    the whole point of this module is to get around syrup auto relations duh

--- JACKED/Syrup/models/DatasBeardRow.php
+++ JACKED/Syrup/models/DatasBeardRow.php
@@ -5,7 +5,7 @@
         const tableName = 'DatasBeardRow';
 
         protected $uuid = array(SyrupField::VARCHAR, 64, false, NULL, 'PRI', NULL, array('UUID'));
-        protected $Table = array(SyrupField::VARCHAR, 64, false, NULL, 'FK', array('hasOne' => 'DatasBeardTable.uuid'));
+        protected $Table = array(SyrupField::VARCHAR, 64, false);
         protected $edited = array(SyrupField::INT, 11, false);
         protected $alive = array(SyrupField::TINYINT, 1, true, 1);
         protected $content = array(SyrupField::BLOB);
