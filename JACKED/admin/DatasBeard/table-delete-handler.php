dd405fbe6c4f721fe6418ef56972bef824c6f3a8
Nick Pettazzoni <pettazz@gmail.com>
2015-08-27 13:00:04 -0400

    dont add super unnecessary namespacing to exceptions

--- JACKED/admin/DatasBeard/table-delete-handler.php
+++ JACKED/admin/DatasBeard/table-delete-handler.php
@@ -4,12 +4,12 @@
         $JACKED->loadDependencies(array('DatasBeard'));
         if($_POST['table-action'] === 'delete' && isset($_POST['table-id'])){
             $JACKED->DatasBeard->deleteTable(trim($_POST['table-id']));
-            $JACKED->Sessions->write('admin.datasbeard.deletetable.success', 'Table deleted successfully.');
+            $JACKED->Sessions->write('admin.datasbeard.success', 'Table deleted successfully.');
         }else{
             throw new Exception("No action defined.");
         }
     }catch(Exception $e){
-        $JACKED->Sessions->write('admin.datasbeard.deletetable.error', $e->getMessage());
+        $JACKED->Sessions->write('admin.datasbeard.error', $e->getMessage());
     }
 
     include('menu.php');
