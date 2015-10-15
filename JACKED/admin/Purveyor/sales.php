c2892b64a5db6b5e90421564a5b232e4e04ca7a4
Nick Pettazzoni <pettazz@gmail.com>
2015-10-15 18:55:05 -0400

    fixes #36 and #37 - add filtering and date range limiting to Sale page

--- JACKED/admin/Purveyor/sales.php
+++ JACKED/admin/Purveyor/sales.php
@@ -3,32 +3,145 @@
     $JACKED->loadDependencies(array('Syrup'));
 
 ?>
+<link href="<?php echo $JACKED->admin->config->entry_point; ?>assets/js/daterangepicker/daterangepicker.css" rel="stylesheet" />
+
+<style type="text/css">
+    #spinnerContainer{
+        display: block;
+        position: relative;
+        height: 75px;
+        width: 100%;
+    }
+
+    #filters div{
+        margin-right: 40px;
+    }
+</style>
+
+<script type="text/javascript" src="<?php echo $JACKED->admin->config->entry_point; ?>assets/js/moment.min.js"></script>
+<script type="text/javascript" src="<?php echo $JACKED->admin->config->entry_point; ?>assets/js/jquery.ba-throttle-debounce.min.js"></script>
+<script type="text/javascript" src="<?php echo $JACKED->admin->config->entry_point; ?>assets/js/daterangepicker/daterangepicker.js"></script>
 
 <script type="text/javascript">
     
     $(document).ready(function(){
-        $(".saleDetailsOpen").click(function(eo){
+        $('input[name="daterange"]').val(moment(), moment());
+        updateRows();
+
+        $("#filters").submit(function(eo){
             eo.preventDefault();
-            $('#detailsRow-' + $(this).attr('data-guid')).show();
+            updateRows();
+            return false;
         });
 
-        $(".saleDetailsClose").click(function(eo){
-            eo.preventDefault();
-            $('#detailsRow-' + $(this).attr('data-guid')).hide();
+        $("#filters input").change(function(){
+            updateRows();
+        });
+
+        $("input[name=filter]").keyup($.debounce( 500, function(){
+            updateRows();
+        }));
+
+        $("#clearFilter").click(function(){
+            $("input[name=filter]").val('');
+            updateRows();
         });
 
         $('#collectTrackingModal').on('hidden', function(){
             $("form#shippingUpdate input#saleGuid").val('');
-        })
+        });
 
-        $(".markShipped").click(function(eo){
-            eo.preventDefault();
-            $("form#shippingUpdate input#saleGuid").val($(this).attr('data-guid'));
-            $('#collectTrackingModal').modal({
-                keyboard: true
+        $('input[name="daterange"]').daterangepicker({
+            buttonClasses: "btn btn-small",
+            applyClass: "btn btn-success",
+            cancelClass: "btn btn-default",
+            ranges: {
+               'Today': [moment(), moment()],
+               'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+               'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+               'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+               'This Month': [moment().startOf('month'), moment().endOf('month')],
+               'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            }
+        });
+        $('input[name="daterange"]').change(updateRows);
+    });
+
+    function updateRows(){
+        var dateConstraint = $('#searchWithinDateRange').prop('checked');
+        var filterValue = $("input[name=filter]").val();
+        var filterActive = $.trim(filterValue) !== '';
+
+        $('form#filters input,button').prop('disabled', true);
+
+        $('tbody').html('');
+        $('#spinnerContainer').show();
+        var opts = {
+          lines: 15// The number of lines to draw
+          , length: 15 // The length of each line
+          , width: 1 // The line thickness
+          , radius: 15 // The radius of the inner circle
+          , scale: 1 // Scales overall size of the spinner
+          , corners: 0.5 // Corner roundness (0..1)
+          , color: '#000' // #rgb or #rrggbb or array of colors
+          , opacity: 0.0 // Opacity of the lines
+          , rotate: 0 // The rotation offset
+          , direction: 1 // 1: clockwise, -1: counterclockwise
+          , speed: 1.2 // Rounds per second
+          , trail: 50 // Afterglow percentage
+          , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+          , zIndex: 2e9 // The z-index (defaults to 2000000000)
+          , className: 'spinner' // The CSS class to assign to the spinner
+          , top: '50%' // Top position relative to parent
+          , left: '50%' // Left position relative to parent
+          , shadow: false // Whether to render a shadow
+          , hwaccel: true // Whether to use hardware acceleration
+          , position: 'absolute' // Element positioning
+        }
+        var target = $('#spinnerContainer')[0];
+        var spinner = new Spinner(opts).spin(target);
+
+        var data = {};
+        if(filterActive){
+            if(dateConstraint){
+                data['dateRange'] = $('input[name=daterange]').val();
+            }
+            data['filter'] = filterValue;
+        }else{
+            data['dateRange'] = $('input[name=daterange]').val();
+        }
+
+        $.get('<?php echo $JACKED->admin->config->entry_point; ?>handler/salesfetch', data)
+        .done(function(data){
+            spinner.stop();   
+            $('#spinnerContainer').hide();         
+            $('form#filters input,button').prop('disabled', false);
+
+            if($.trim(data)){
+                $('tbody').html(data);
+            }else{
+                $('tbody').html('<tr><td colspan="7"><p class="lead">No Sales matched filters</p></td></tr>');
+            }
+
+            $(".saleDetailsOpen").click(function(eo){
+                eo.preventDefault();
+                $('#detailsRow-' + $(this).attr('data-guid')).show();
+            });
+
+            $(".saleDetailsClose").click(function(eo){
+                eo.preventDefault();
+                $('#detailsRow-' + $(this).attr('data-guid')).hide();
+            });
+
+            $(".markShipped").click(function(eo){
+                eo.preventDefault();
+                $("form#shippingUpdate input#saleGuid").val($(this).attr('data-guid'));
+                $('#collectTrackingModal').modal({
+                    keyboard: true
+                });
             });
         });
-    });
+    }
 </script>
 
 
@@ -82,6 +195,22 @@
     }
 
 ?>
+<form class="form-inline" id="filters">
+    <div class="input-prepend input-append">
+      <span class="add-on"><i class="icon-calendar"></i></span>
+      <input type="text" name="daterange" value="" />
+    </div>
+
+    <div class="input-prepend input-append">
+      <span class="add-on"><i class="icon-search"></i></span>
+      <input type="text" name="filter" placeholder="Search Anything" />
+      <button id="clearFilter" class="btn" type="button">Clear</button>
+    </div>
+
+    <label class="checkbox">
+        <input id="searchWithinDateRange" type="checkbox" checked> Search only within selected date range
+    </label>
+</form>
 
 <table class="table">
     <thead>
@@ -96,139 +225,6 @@
         </tr>
     </thead>
     <tbody>
-
-<?php
-    
-    $sales = $JACKED->Syrup->Sale->find(array(), array('field' => 'timestamp', 'direction' => 'DESC'));
-    foreach($sales as $sale){
-        $paymentSymbol = ($sale->payment == 'DOGE'? '<strong>Ð</strong>' : '<strong>$</strong>');
-        $paymentTotal = ($sale->payment == 'DOGE'? floor($sale->converted_total) : sprintf("%01.2f", ($sale->total / 100.0)));
-        if($sale->external_transaction_id){
-            if($sale->confirmed){
-                $confirmationState = '<span class="label label-success"><i class="icon-white icon-ok-sign"></i></span>';
-            }else{
-                $confirmationState = '<span class="label label-warning"><i class="icon-white icon-time"></i></span>';
-            }
-        }else{
-            $confirmationState = '<span class="label label-important"><i class="icon-white icon-remove-sign"></i></span>';
-        }
-        echo '    <tr>';
-        echo '            <td> ' . date('m/d/Y', $sale->timestamp) . '</td>';
-        echo '            <td> <a href="mailto:' . $sale->User->email . '">' . $sale->User->email . '</a></td>';
-        echo '            <td> ' . $sale->quantity . '</td>';
-        echo '            <td> ' . $sale->Product->name . '</td>';
-        echo '            <td> ' . $confirmationState . ' ' . $paymentSymbol . $paymentTotal . '</td>';
-        echo '            <td> <i class="icon-' . ($sale->shipped? 'ok' : 'remove') . '"></i></td>';
-        echo '            <td class="actionsrow">
-            <div class="btn-group">
-              <a class="btn btn-primary dropdown-toggle" data-toggle="dropdown" href="#">
-                <i class="icon-tasks icon-white"></i>
-                <span class="caret"></span>
-              </a>
-              <ul class="dropdown-menu">
-                <li><a data-guid="' . $sale->guid . '" href="#" class="saleDetailsOpen"><i class="icon-eye-open"></i> Show Details</a></li>
-                ' . ($sale->Product->tangible && !$sale->shipped? '<li><a href="" class="markShipped" data-guid="' . $sale->guid . '"><i class="icon-plane"></i> Mark as Shipped</a></li>' : '') . '
-                <li><a href=""><i class="icon-ban-circle"></i> Cancel/Refund Sale</a></li>
-              </ul>
-            </div>
-        </td>';
-        echo '    </tr>';
-
-        echo '    <tr id="detailsRow-' . $sale->guid . '" style="display:none;">';
-        echo '      <td colspan="4">';
-        echo '
-        <dl class="dl-horizontal">
-            <dt>JACKED Sale ID</dt>
-            <dd class="guid">' . $sale->guid . '</dd>
-            
-            <dt>User</dt>
-            <dd class="user">' . $sale->User->email . '</dd>
-            
-            <dt>Timestamp</dt>
-            <dd class="timestamp">' . date('D m/d/Y H:i:s', $sale->timestamp) . '</dd>
-            
-            <dt>Product</dt>
-            <dd class="product">' . $sale->Product->name . '</dd>
-            
-            <dt>Quantity</dt>
-            <dd class="quantity">' . $sale->quantity . '</dd>
-            
-            <dt>Total Paid</dt>
-            <dd class="total">' . $paymentSymbol . $paymentTotal . '</dd>
-            
-            <dt>Total Price</dt>
-            <dd class="total"> <strong>$</strong>' . sprintf("%01.2f", ($sale->total / 100.0)) . '</dd>
-            
-            <dt>Payment Confirmation</dt>
-            <dd class="confirmed"><i class="icon-' . ($sale->confirmed? 'ok' : 'remove') . '"></i></dd>
-            
-            <dt>Confirmation Updated</dt>
-            <dd>' . ($sale->IPN_timestamp > 0? date('D m/d/Y H:i:s', $sale->IPN_timestamp) : 'N/A') . '</dd>
-            
-            <dt>Payment Method</dt>
-            <dd class="payment">' . $sale->payment . '</dd>
-        
-            <dt>Shipped</dt>
-            <dd class="shipped"><i class="icon-' . ($sale->shipped? 'ok' : 'remove') . '"></i></dd>
-            
-            <dt>Shipping Tracker</dt>
-            <dd class="tracking">' . ($sale->tracking? '<a href="https://www.packagetrackr.com/track/' . $sale->tracking . '" target="_blank">' . $sale->tracking . '</a>' : 'N/A') . '</dd>
-            
-            <dt>External Transaction ID</dt>
-            <dd class="external">' . ($sale->external_transaction_id? $sale->external_transaction_id : 'N/A') . '</dd>';
-
-        if($sale->dimensions){
-            foreach(json_decode($sale->dimensions) as $dimKey => $dimValue){
-                echo '<dt>' . $dimKey . '</dt>';
-                echo '<dd>' . $dimValue . '</dd>';
-            }
-        }
-
-        echo '
-        </dl>
-        <a data-guid="' . $sale->guid . '" href="#" class="btn btn-primary saleDetailsClose"><i class="icon-eye-close icon-white"></i> Hide Details</a>
-      </td>';
-        if($sale->Product->tangible){
-            echo '<td colspan="3">';
-            echo '
-            <dl class="dl-horizontal">
-                <dt>Shipping Details</dt>
-                <dd><small>' . $sale->User->guid . '</small></dd>
-
-                <dt>Recipient Name</dt>
-                <dd>' . $sale->ShippingAddress->recipient_name . '</dd>
-                
-                <dt>Type</dt>
-                <dd>' . $sale->ShippingAddress->type . '</dd>
-                
-                <dt>Line 1</dt>
-                <dd>' . $sale->ShippingAddress->line1 . '</dd>
-
-                <dt>Line 2</dt>
-                <dd>' . ($sale->ShippingAddress->line2? $sale->ShippingAddress->line2 : 'N/A') . '</dd>
-
-                <dt>City</dt>
-                <dd>' . $sale->ShippingAddress->city . '</dd>
-
-                <dt>State</dt>
-                <dd>' . $sale->ShippingAddress->state . '</dd>
-
-                <dt>Postal Code</dt>
-                <dd>' . $sale->ShippingAddress->postal_code . '</dd>
-
-                <dt>Country</dt>
-                <dd>' . $sale->ShippingAddress->country . '</dd>
-
-                <dt>Phone Number</dt>
-                <dd>' . $sale->ShippingAddress->phone . '</dd>
-
-            </dl>
-            ';
-            echo '</td>';
-        }
-
-        echo '    </tr>';
-    }
-?>
     </tbody>
 </table>
+<div id="spinnerContainer"></div>
