203a3f7fc786f08fac74197d35c73f273a1a6fa1
Nick Pettazzoni <pettazz@gmail.com>
2015-10-21 12:39:03 -0400

    this thing needs to default to whatever JACKED uses, not rely on ini and then globally override

--- JACKED/lib/paypal-vendor/paypal/rest-api-sdk-php/lib/PayPal/Core/PayPalLoggingManager.php
+++ JACKED/lib/paypal-vendor/paypal/rest-api-sdk-php/lib/PayPal/Core/PayPalLoggingManager.php
@@ -71,7 +71,7 @@ class PayPalLoggingManager
     public function __construct()
     {
         // To suppress the warning during the date() invocation in logs, we would default the timezone to GMT.
-        if (!ini_get('date.timezone')) {
+        if (!date_default_timezone_get()) {
             date_default_timezone_set('GMT');
         }
 
